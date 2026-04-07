3e82c9438c7208ed57722d519a83a6595bea7159
Ankur Gupta <68587598+ankur-gupta-29@users.noreply.github.com>
2026-04-08 01:37:56 +0530

    Update lorenz_motion.v

--- src/lorenz_motion.v
+++ src/lorenz_motion.v
@@ -4,7 +4,7 @@
  */
 `default_nettype none
 
-module tt_um_lorenz_motion_4x2(
+module tt_um_lorenz_motion(
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
